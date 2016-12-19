d3e944b2690f715ebd28a03ef24e6e78b412def9
Sachin Shetty <scn.3089@gmail.com>
2016-12-19 17:42:23 +0530

    tables added for UI

--- mysql/ddl.sql
+++ mysql/ddl.sql
@@ -16,3 +16,30 @@ create table twitterlist
   screen_name varchar(50),
   twitterid varchar(50)
 );
+
+create table restaurantnames
+(
+  Id bigint,
+  name varchar(2000),
+  latitude float,
+  longitude float,
+  street varchar(100),
+  zip varchar(100)
+);
+
+create table stationnames(
+  name varchar(500),
+  latitude float,
+  longitude float
+);
+
+create table stationrest(
+  id bigint,
+  name varchar(1000),
+  type varchar(100),
+  stationname varchar(1000),
+  latitude float,
+  longitude float,
+  site varchar(1000),
+  epoch varchar(1000)
+);
