caa421a78a6a17b1608450e06fb066a6a7715b09
Sachin Shetty <scn.3089@gmail.com>
2016-12-29 18:27:30 +0530

    Update daily_fbingest.sh

--- scripts/daily_fbingest.sh
+++ scripts/daily_fbingest.sh
@@ -9,6 +9,11 @@ hadoop fs -test -d /grosvenor/facebook/facebooktopic/$fdate
 if [ $? != 0 ]
 then nohup node /opt/nodeprojects/facebooksearch/facebookgraphapi.js $fdate &
 else 
-hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/FacebookData.* 
+hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/fb_* 
 nohup node /opt/nodeprojects/facebooksearch/facebookgraphapi.js $fdate &   
 fi
+
+#merge files
+hadoop fs -cat /grosvenor/facebook/facebooktopic/$fdate/*.* | hadoop fs -put - /grosvenor/facebook/facebooktopic/$fdate/fb_$fdate.txt
+#purge files
+hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/FacebookData.*
