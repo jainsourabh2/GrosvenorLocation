684aa429ad5fb44c2826ea82ad1446c85b9d33c3
Sachin Shetty <scn.3089@gmail.com>
2016-12-30 17:42:54 +0530

    Update fb_hive_partitions.sh

--- Migration_script/fb_hive_partitions.sh
+++ Migration_script/fb_hive_partitions.sh
@@ -1,27 +1,17 @@
 #!/bin/sh
-
-
-startdate=`/bin/date --date="11 month ago" +%Y-%m-%d`
-startmonth=`/bin/date --date="11 month ago" +%Y-%m`
+startdate=`/bin/date  +2016-01-01`
 #echo $startdate $startmonth
 
-currentdate=`/bin/date +%Y-%m-%d`
-currentmonth=`/bin/date +%Y-%m`
-
+currentdate=`/bin/date +2016-12-30`
 #echo $currentdate $currentmonth
 
 foldate="$startdate"
-folmonth="$startmonth"
-#echo $foldate  $folmonth
-#echo $foldate $currentdate
+
 while [ $(date -d "$foldate" +%Y%m%d) -le $(date -d "$currentdate" +%Y%m%d) ]
 do
-  echo $folmonth
-          new_command=$" hive -e \"ALTER TABLE facebookdata ADD PARTITION(fb_date='$folmonth') location '/grosvenor/facebook/facebooktopic/$folmonth';\" "    
+  echo $foldate
+          new_command=$" hive -e \"ALTER TABLE facebookdata ADD PARTITION(fb_date='$foldate') location '/grosvenor/facebook/facebooktopic/$foldate';\" "    
           printf "New command %s\n" "$new_command"
           eval $new_command
-foldate=`/bin/date --date="$foldate 1 month" +%Y-%m-%d`
-folmonth=`/bin/date --date="$foldate" +%Y-%m`
-#echo $foldate  $folmonth
-#echo $foldate $currentdate
+foldate=`/bin/date --date="$foldate 1 day" +%Y-%m-%d`
 done
