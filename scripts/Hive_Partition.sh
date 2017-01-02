bb2e0a69ae1c7bc1d7e3d0e6dbbc58695b6e894d
Sachin Shetty <scn.3089@gmail.com>
2017-01-02 12:28:58 +0530

    added fb date partitions

--- scripts/Hive_Partition.sh
+++ scripts/Hive_Partition.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 new_date=$(date -d "+1 days" +'%y-%m-%d')
+next_dt=$(date -d "+1 days" +'%Y-%m-%d')
 printf "Next Date in YY-MM-DD format %s\n" "$new_date"
 new_command=$" hive -e \"ALTER TABLE twitterstream ADD PARTITION(create_date='$new_date') location '/grosvenor/twitter/grosvenorkafkaflume/$new_date';\" "
 printf "New command %s\n" "$new_command"
-
+fb_command=$" hive -e \"ALTER TABLE facebookdata ADD PARTITION(fb_date='$next_dt') location '/grosvenor/facebook/facebooktopic/$next_dt' ;\" "
 eval $new_command
+eval $fb_command
