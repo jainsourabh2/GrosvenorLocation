74ef3cda56862d2e7cc6ab102174bfc91e086fcb
Sachin Shetty <scn.3089@gmail.com>
2017-01-04 15:07:08 +0530

    script for previous fb data

---
+++ scripts/bulkFBdata/bulkhivepartition.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+startdate=`/bin/date  +2016-01-01`
+#echo $startdate $startmonth
+
+currentdate=`/bin/date +2016-12-30`
+#echo $currentdate $currentmonth
+
+foldate="$startdate"
+
+while [ $(date -d "$foldate" +%Y%m%d) -le $(date -d "$currentdate" +%Y%m%d) ]
+do
+  echo $foldate
+          new_command=$" hive -e \"ALTER TABLE facebookdata ADD PARTITION(fb_date='$foldate') location '/grosvenor/facebook/facebooktopic/$foldate';\" "    
+          printf "New command %s\n" "$new_command"
+          eval $new_command
+foldate=`/bin/date --date="$foldate 1 day" +%Y-%m-%d`
+done
