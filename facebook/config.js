a81f87d1c546ddbccae251209d059d06278b4cfb
Sachin Shetty <scn.3089@gmail.com>
2017-01-04 14:52:01 +0530

    Update config.js

--- facebook/config.js
+++ facebook/config.js
@@ -1,15 +1,16 @@
 module.exports.constants = {
-	mysql_username	: 'n00bc0der',
+	mysql_username	: '',
 	mysql_password	: '',
-	mysql_database	: 'c9',
-	mysql_host		: '0.0.0.0',
-	mysql_port      : "3306",
+	mysql_database	: '',
+	mysql_host		: '',
+	//mysql_port      : "3306",
 	facebook_app_id :   "",
 	facebook_app_secret:  "",
-	access_token: ""
+	facebook_access: []
 }
-
 module.exports.middlewareconstants = {
-	fieldDelimter	:	'|'
+	fieldDelimter	:	'|',
+	facebooksearchtype: 'Bars', 
+       facebooklocations:'ip/locations.csv'
 } 
 
