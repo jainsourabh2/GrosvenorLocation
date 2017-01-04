85a889a470a927d992cb6fb6123256246d8a75ea
Sachin Shetty <scn.3089@gmail.com>
2017-01-04 15:08:48 +0530

    Create pigshell.sh

---
+++ scripts/bulkFBdata/pigshell.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+startdate=`/bin/date  +2016-01-01`
+currentdate=`/bin/date  +2016-02-29`
+
+foldate="$startdate"
+
+echo $foldate $currentdate
+
+while [ $(date -d "$foldate" +%Y%m%d) -le $(date -d "$currentdate" +%Y%m%d) ]
+do
+
+#check if folder exists in hadoop
+     hadoop fs -test -d /grosvenor/facebook/facebooktopic/$foldate
+     if [ $? != 0 ]
+     then
+          echo "create $foldate directory in hadoop"
+          pig -param date=$foldate  /opt/nodeprojects/GrosvenorLocation/facebooksearch/shellscript/pigscript.pig
+     else
+          hadoop fs -rmr /grosvenor/facebook/facebooktopic/$foldate
+          pig -param date=$foldate  /opt/nodeprojects/GrosvenorLocation/facebooksearch/shellscript/pigscript.pig
+    
+     fi
+     #merge the files
+     hadoop fs -cat /grosvenor/facebook/facebooktopic/$foldate/part-m-* | hadoop fs -put -  /grosvenor/facebook/facebooktopic/$foldate/fb_$foldate.txt
+
+     #purge the files
+     hadoop fs -rm /grosvenor/facebook/facebooktopic/$foldate/part-m-*
+     hadoop fs -rm /grosvenor/facebook/facebooktopic/$foldate/_*
+foldate=`/bin/date --date="$foldate 1 day" +%Y-%m-%d`
+done
