843d226a59d324f2e2fedd7cd8a89f6c9d117b14
Sachin Shetty <scn.3089@gmail.com>
2017-01-04 15:02:52 +0530

    Update mergeHadoopFiles.sh

--- scripts/mergeHadoopFiles.sh
+++ scripts/mergeHadoopFiles.sh
@@ -9,12 +9,3 @@ eval $HDFS_COMMAND_MERGE
 echo $
 eval $HDFS_COMMAND_PURGE
 echo $
-
-startdate=`/bin/date +%Y-%m-%d`
-FB_BASE_PATH="/grosvenor/facebook/facebooktopic/$startdate"
-HDFS_CMD_MERGE="hdfs dfs -cat $FB_BASE_PATH/*.* | hdfs dfs -put - $FB_BASE_PATH/fb_$startdate.txt"
-HDFS_CMD_PURGE="hdfs dfs -rmr $FB_BASE_PATH/FacebookData.*"
-eval $HDFS_CMD_MERGE
-echo $
-eval $HDFS_CMD_PURGE
-echo $
