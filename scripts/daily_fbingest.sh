9c45833f41a748e7276c0bc49c17dee960776a29
Sachin Shetty <scn.3089@gmail.com>
2017-01-05 16:01:48 +0530

    Update daily_fbingest.sh

--- scripts/daily_fbingest.sh
+++ scripts/daily_fbingest.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 #echo $startdate
+currenthour=`/bin/date +%H`
+echo $currenthour
+
+#At 12 in morning run
+if [ $currenthour == 0 ]
+then
+currentdate=`/bin/date --date="1 days ago" +%Y-%m-%d`
+else 
 currentdate=`/bin/date +%Y-%m-%d`
+fi
 fdate="$currentdate"
 echo $fdate
 #check if folder exists in hadoop
 
 hadoop fs -test -d /grosvenor/facebook/facebooktopic/$fdate
 if [ $? != 0 ]
-then nohup node /opt/nodeprojects/facebooksearch/facebookgraphapi.js $fdate &
+then 
+nohup node /opt/nodeprojects/GrosvenorLocation/facebooksearch/facebookgraphapi.js $fdate &
 else 
-hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/fb_* 
-nohup node /opt/nodeprojects/facebooksearch/facebookgraphapi.js $fdate &   
+hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/FacebookData.* 
+nohup node /opt/nodeprojects/GrosvenorLocation/facebooksearch/facebookgraphapi.js $fdate &   
 fi
 
-#merge files
-hadoop fs -cat /grosvenor/facebook/facebooktopic/$fdate/*.* | hadoop fs -put - /grosvenor/facebook/facebooktopic/$fdate/fb_$fdate.txt
-#purge files
-hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/FacebookData.*
+
