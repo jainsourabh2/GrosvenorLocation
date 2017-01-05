aefb9bade574568c84030ec53d76c35ec40cc333
Sachin Shetty <scn.3089@gmail.com>
2017-01-05 16:02:32 +0530

    Update mergecmd.sh

--- scripts/mergecmd.sh
+++ scripts/mergecmd.sh
@@ -1,4 +1,4 @@
-startdate=`/bin/date +%Y-%m-%d`
+startdate=`/bin/date --date="1 days ago" +%Y-%m-%d`
 FB_BASE_PATH="/grosvenor/facebook/facebooktopic/$startdate"
 HDFS_CMD_MERGE="hdfs dfs -cat $FB_BASE_PATH/*.* | hdfs dfs -put - $FB_BASE_PATH/fb_$startdate.txt"
 HDFS_CMD_PURGE="hdfs dfs -rmr $FB_BASE_PATH/FacebookData.*"
