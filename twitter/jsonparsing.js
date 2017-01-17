960e0d1defa7a0051873b306f13218b5bc5fdfda
Sachin Shetty <scn.3089@gmail.com>
2017-01-17 15:40:44 +0530

    category and sentiment field added

--- twitter/jsonparsing.js
+++ twitter/jsonparsing.js
@@ -4,9 +4,15 @@ const sanitizeHtml = require('sanitize-html');
 const config = require('../config/config');
 const constants = config.middlewareconstants;
 const jsonParsing = {};
+const fs = require('fs');
+const sentiment = require("sentiment");
+
+var abusiveflag = false;
+
+
 (function(jsonParsing){
 
-  jsonParsing.getParsedString = function(inputString){
+  jsonParsing.getParsedString = function(inputString,abusivelist,iscategorised){
 	var idList = ["id_str","created_at","text","source","in_reply_to_status_id_str","in_reply_to_user_id_str","in_reply_to_screen_name","user.id_str","user.name","user.screen_name","user.location","user.description","user.url","user.protected","user.verified","user.followers_count","user.friends_count","user.statuses_count","user.friends_count","user.listed_count","user.favourites_count","user.created_at","user.geo_enabled","user.lang","user.profile_image_url","user.default_profile","user.default_profile_image","geo.coordinates[0]","coordinates.coordinates[0]","retweet_count","favorite_count","retweeted","favorited","filter_level","lang","place.place_type","place.name","place.full_name","place.country_code","place.country","place.bounding_box.coordinates[0][0]","place.bounding_box.coordinates[0][1]","place.bounding_box.coordinates[0][2]","place.bounding_box.coordinates[0][3]","entities.hashtags[0].text","entities.user_mentions[0].screen_name","entities.urls[0].url"];
 	var input = sanitizeHtml(inputString, {
 		allowedTags: [],
@@ -44,18 +50,58 @@ const jsonParsing = {};
 				evalVal = eval(val);
 				if(val == 'parsedJSON.user.description' || val == 'parsedJSON.text'){
 					evalVal = evalVal.replace(/(?:\r\n|\r|\n)/g,' ').trim();
+					
+					if(iscategorised && abusivelist.length > 0)
+					{
+						for(var al = 0; al < abusivelist.length ; al++)
+						{
+							if(evalVal.split(' ').indexOf(abusivelist[al]) > -1)
+							{
+								abusiveflag = true;
+								break;
+							}
+						}
+					
+					}
 				}
 			}
 		}catch(ex){
-			evalVal = '';
+			evalVal = null;
 		}
 		if(evalVal === undefined || evalVal === null) {
 			output = output + 'null' + constants.fieldDelimter;
 		}else{
 			output = output + evalVal + constants.fieldDelimter;
 		}
+		
+		// Append sentiment score after tweet text
+		if(iscategorised && val == 'parsedJSON.text')
+		{
+			var sentimentscore = '0';
+			if(evalVal != null)
+			{
+			  sentimentscore = sentiment(evalVal).score;	
+			}
+			
+			output = output + sentimentscore + constants.fieldDelimter;
+		}
 	}
+	
+	if(iscategorised)
+	{
+		if(abusiveflag)
+		{
+			output = output + "1" + constants.fieldDelimter;	 //Abusive words categorised
+		}
+		else
+		{
+			output = output + "0" + constants.fieldDelimter;     //Does not categorised as abusive
+		}
+		abusiveflag = false;
+	}
+	
 	output = output.substr(0,output.length-1);
+	console.log(output);
 	return output;
   };
 
