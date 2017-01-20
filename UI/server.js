94195de85983b0eb706cc4afffc45c6d67106a8a
Sachin Shetty <scn.3089@gmail.com>
2017-01-20 11:09:21 +0530

    Update server.js

--- UI/server.js
+++ UI/server.js
@@ -3,12 +3,20 @@ var app = express();
 
 //***** Routing call ********
 var router = require("./router/main");
-router(app);
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static('images'));
+
 //****** Engine Logic Here ************* //
 app.set('view engine','ejs');
 app.engine('html', require('ejs').renderFile); //Specify view engine 
 
+router(app);
 var server = app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0",function(){
 	console.log("Started server on port 3000 !!!");
 });
