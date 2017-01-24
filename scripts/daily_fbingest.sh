310abd12e3dc4b77f2807922f99115af140f3c9a
Sachin Shetty <scn.3089@gmail.com>
2017-01-24 15:42:23 +0530

    Update daily_fbingest.sh

--- scripts/daily_fbingest.sh
+++ scripts/daily_fbingest.sh
@@ -18,10 +18,10 @@ ps -ef | grep facebookgraphapi.js | grep -v grep | awk '{print $2}' | xargs kill
 hadoop fs -test -d /grosvenor/facebook/facebooktopic/$fdate
 if [ $? != 0 ]
 then 
-nohup node /opt/nodeprojects/GrosvenorLocation/facebooksearch/facebookgraphapi.js $fdate &
+nohup node /opt/nodeprojects/GrosvenorLocation/facebook/facebookgraphapi.js $fdate &
 else 
 hadoop fs -rm /grosvenor/facebook/facebooktopic/$fdate/FacebookData.* 
-nohup node /opt/nodeprojects/GrosvenorLocation/facebooksearch/facebookgraphapi.js $fdate &   
+nohup node /opt/nodeprojects/GrosvenorLocation/facebook/facebookgraphapi.js $fdate &   
 fi
 
 
