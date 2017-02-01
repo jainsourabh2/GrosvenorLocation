ae1f48b638b2ac085a08e0d6b315f523a88d5eb4
Sachin Shetty <scn.3089@gmail.com>
2017-02-01 10:51:02 +0530

    Update config.js

--- UI/router/config.js
+++ UI/router/config.js
@@ -1,10 +1,12 @@
 module.exports.constants = {
-	mysql_username	: 'n00bc0der',
-	mysql_password	: '',
-	mysql_database	: 'c9',
-	mysql_host		: '0.0.0.0',
-	mysql_port      : "3306"
+mysql_username	: 'root',
+mysql_password	: '',
+mysql_database	: '',
+mysql_host	: '',
+mssql_host : '',
+mssql_database : '',
+mssql_username : '',
+mssql_password : '',
+mssql_port : ''
+
 }
-
-
-
