3aea42be378abd3ccc9b32e9ae9f081aedf47857
Sachin S. Shetty <Sachin13031@Mastek.com>
2017-02-03 10:56:33 +0530

    Code from Web Server with config added

---
+++ UI/config/config.js
@@ -0,0 +1,41 @@
+module.exports.constants = {
+	
+
+mysql_username	: 'root',
+
+
+mysql_password	: '',
+
+
+mysql_database	: 'grosvenor',
+
+
+mysql_host	: '10.80.2.4',
+
+
+mssql_host : '10.80.2.5',
+
+
+mssql_database : 'BigDataStore',
+
+
+mssql_username : 'mastek',
+
+
+mssql_password : 'M8$tek12',
+
+
+mssql_port : '55413',
+
+logDir	: 'C:\\apilogs',
+
+//logDir	: '/var/log/grosvenor/',
+
+environment : 'development',
+
+null : null,
+
+undefined : undefined
+
+
+}
---
+++ UI/config/log.js
@@ -0,0 +1,51 @@
+//{ error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 }
+'use strict';
+
+const winston = require('winston');
+const fs = require('fs');
+const config = require('./config.js');
+const constants = config.constants;
+const logDir = constants.logDir;
+const env = process.env.NODE_ENV || constants.environment;
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir);
+}
+const tsFormat = () => (new Date()).toLocaleTimeString();
+const logger = new (winston.Logger)({
+  transports: [
+    // colorize the output to the console
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: env === 'development' ? 'verbose' : 'info'
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `${logDir}/-debug.log`,
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      json: false,
+      level: env === 'development' ? 'verbose' : 'info'
+    })
+  ],
+  exceptionHandlers: [
+    new (winston.transports.Console)({
+      timestamp: tsFormat,
+      colorize: true,
+      level: env === 'development' ? 'verbose' : 'info'
+    }),
+    new (require('winston-daily-rotate-file'))({
+      filename: `${logDir}/-exceptions.log`,
+      timestamp: tsFormat,
+      datePattern: 'yyyy-MM-dd',
+      prepend: true,
+      json: false,
+      level: env === 'development' ? 'verbose' : 'info'
+    })
+  ],
+  exitOnError: false
+});
+
+module.exports = logger;
