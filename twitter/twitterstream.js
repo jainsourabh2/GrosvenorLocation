170d6caca1841da0b586eae86cadaaf017a37a52
Sachin S. Shetty <Sachin13031@Mastek.com>
2017-08-03 17:11:33 +0530

    More Keywords to track added

--- twitter/twitterstream.js
+++ twitter/twitterstream.js
@@ -57,7 +57,7 @@ readFileFunc('/opt/nodeprojects/GrosvenorLocation/config/abusivewords').then(fun
    abusivelist = data.split('\n');
 	
 	
-	connection.query('SELECT twitterid FROM twitterlist WHERE twitterid IS NOT null', function(err, rows, fields) {
+	connection.query('SELECT twitterid FROM twitterlist WHERE twitterid IS NOT null limit 1000', function(err, rows, fields) {
 	if (err){
 		throw err;
 	}
@@ -71,8 +71,16 @@ readFileFunc('/opt/nodeprojects/GrosvenorLocation/config/abusivewords').then(fun
 function startstreaming(){
 	var payloads;
 	var newpayloads;
-	//Location boundary coordinate for London (-0.5103, 51.2868, 0.3340, 51.6923), for liverpool(-3.022091,53.377726,-2.876204,53.447641) 
-	client.stream('statuses/filter', {follow: twitterIdList,locations:'-0.5103, 51.2868, 0.3340, 51.6923,-3.022091,53.377726,-2.876204,53.447641',track:'Grosvenor,Mayfair,Belgravia,LiverpoolONE'}, function(stream) {
+        
+	var keywords_to_track = "Grosvenor,Mayfair,Belgravia,LiverpoolONE,North Mayfair,North Audley Street,Duke Street,Mount Street,South Audley Street,Carlos Place,Davies Street,Grosvenor Hill,South Molton Street,Grosvenor Street,Brook Street,Grosvenor Square,Brown Hart Gardens,Mount Street Gardens,summer in the square,scotts mount street,balenciaga mount street,celine mount street,mount street mayfair,mount street gardens,christian louboutin mount street,mount street restaurants,mount street shops,georg jensen mount street,marc jacobs mount street,the audley mount street,lanvin mount street,christopher kane mount street,gianvito rossi mount street,mount street cafe,linda farrow mount street,sophia webster mount street,roksanda mount street,carolina herrera mount street,oscar de la renta mount street,simone rocha mount street,8 mount street london,stephen webster mount street,creed mount street,pringle of scotland mount street,mount street celine,george mount street,mayfair shops,mount street mayfair,mayfair shopping,mayfair boutiques,mayfair property,mayfair office,mayfair apartments london,mayfair flats,property in mayfair,mayfair property prices,apartments in mayfair,mayfair real estate,property mayfair,mayfair property management,commercial property mayfair,houses mayfair,mayfair residential,mayfair london properties,mayfair restaurants,restaurants mayfair,mayfair bar,bars in mayfair,pubs in mayfair,clubs in mayfair,cafe mayfair,mayfair dining,hotel mayfair,5 star hotels mayfair,Motcomb Street,West Halkin Street,Lowndes Street,Kinnerton Street,Elizabeth Street,Ebury Street,Eccleston Yards,Eccleston Street,Pimlico Road,Belgrave Square Garden,Belgrave Square Garden,Eaton Square,Eaton Square Garden,Orange Square,Grosvenor Place,Grosvenor Gardens,Chester Square,belgravia christmas market,belgravia living,belgravia shops,belgravia coffee shop,belgravia market,belgravia shopping,christmas market belgravia,pimlico road farmers market,daylesford pimlico road,pimlico road antiques,orange pimlico road,pimlico road furniture,pimlico road market,pimlico road christmas fair,pimlico road restaurants,restaurants pimlico road,antique shops pimlico road,belgravia property,houses in belgravia,belgravia apartments,belgravia homes,belgravia flats,belgravia townhouse,boisdale belgravia,pubs in belgravia,bars in belgravia,restaurants belgravia,cafe belgravia,hotel belgravia";
+
+	/* Location boundary coordinate for London (-0.5103, 51.2868, 0.3340, 51.6923), for liverpool(-3.022091,53.377726,-2.876204,53.447641), for HK - (113.8142, 22.0875, 114.5119, 22.4732) */
+         
+	var location_to_track = "-0.5103, 51.2868, 0.3340, 51.6923,-3.022091,53.377726,-2.876204,53.447641, 113.8142, 22.0875, 114.5119, 22.4732"
+
+ //Note: follow:twitterIdList giving error as twitterlist is huge so limitng to 1000 for now. Resp error of 431 
+
+	client.stream('statuses/filter', { follow:twitterIdList ,locations: location_to_track ,track:keywords_to_track }, function(stream) {
 	//client.stream('statuses/filter', {follow: twitterIdList}, function(stream) {
 		stream.on('data', function(tweet) {
 			if(tweet.text){
@@ -98,7 +106,7 @@ function startstreaming(){
 		});
 
 		stream.on('error', function(error) {
-			console.log(error);
+			console.log("Error in streaming :" + error);
 		});
 	});
 }
